f474632d77c60194b478acdff53ea4dc8f299734
Veronica Belyakov <122276555+af-vero@users.noreply.github.com>
2025-07-14 15:08:34 +0700

    Update Package.swift for version 1.0.1 release

--- Package.swift
+++ Package.swift
@@ -9,14 +9,13 @@ let package = Package(
     products: [
         .library(
             name: "AppsFlyerMigrationHelper",
-            type: .dynamic,
             targets: ["AppsFlyerMigrationHelper"])
     ],
     targets: [
         .binaryTarget(
             name: "AppsFlyerMigrationHelper",
-            url: "https://github.com/AppsFlyerSDK/appsflyer-apple-migration-helper/releases/download/1.0.0/AppsFlyerMigrationHelper.xcframework.zip",
-            checksum: "e54461fde148da5e551594e4d7f7309465be8f4679041c1390fdf09d8f8fc37b"
+            url: "https://github.com/AppsFlyerSDK/appsflyer-apple-migration-helper/releases/download/1.0.1/AppsFlyerMigrationHelper.xcframework.zip",
+            checksum: "cef1969ff40d35f8d8d60ccc9c6761601702b1b0931ba54b05c4ef9c9fcb560e"
         )
     ]
 )
