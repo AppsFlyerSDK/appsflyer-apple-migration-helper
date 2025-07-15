fd221e4f56a1332c3148d96d3adaead33668f35f
Veronica Belyakov <122276555+af-vero@users.noreply.github.com>
2025-07-15 07:39:34 +0700

    align versions

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AppsFlyerMigrationHelper",
-            url: "https://github.com/AppsFlyerSDK/appsflyer-apple-migration-helper/releases/download/1.0.2/AppsFlyerMigrationHelper.xcframework.zip",
-            checksum: "ccef1969ff40d35f8d8d60ccc9c6761601702b1b0931ba54b05c4ef9c9fcb560e"
+            url: "https://github.com/AppsFlyerSDK/appsflyer-apple-migration-helper/releases/download/1.0.3/AppsFlyerMigrationHelper.xcframework.zip",
+            checksum: "cef1969ff40d35f8d8d60ccc9c6761601702b1b0931ba54b05c4ef9c9fcb560e"
         )
     ]
 )
